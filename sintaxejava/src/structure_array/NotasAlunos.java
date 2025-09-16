3e8eca63e7c4127b8874b3ec616da2b632468535
Guilherme Alves <guilhermepontojesus@gmail.com>
2025-09-16 13:15:22 -0300

    Adicionado conteúdo sobre array em: structure_array

---
+++ sintaxejava/src/structure_array/NotasAlunos.java
@@ -0,0 +1,24 @@
+package structure_array;
+
+public class NotasAlunos {
+    public static double calcularMedia(double[] notas) {
+        double soma = 0;
+        for (double nota : notas) {
+            soma += nota;
+        }
+        return soma / notas.length;
+    }
+
+    public static void main(String[] args) {
+        double[] notas = {5.0, 9.5, 8.0, 4.0, 7.5};
+
+        for (int i = 0; i < notas.length; i++) {
+            System.out.println("Indice: " + i + " = " + notas[i]);
+        }
+
+        double media = calcularMedia(notas);
+        System.out.println("A média é de: " + media);
+
+    }
+}
+
